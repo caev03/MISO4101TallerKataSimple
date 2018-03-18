ff6d618fb8181c412f9adec5cbd1c07e6cc095cf
jshernandez15 <js.hernandez15@uniandes.edu.co>
2018-03-18 17:19:45 -0500

    Paso 3 - refactoring dos numeros (azul)

--- Calculadora.py
+++ Calculadora.py
@@ -8,4 +8,8 @@ class Calculadora:
         if(len(cadena) == 1):
 	    valor = int(cadena)
 	    return [1, valor, valor, valor]
+	if(len(cadena) == 3):
+	    valorU = int(cadena[0])
+	    valorD = int(cadena[2])
+	    return [2,min(valorU,valorD),max(valorU,valorD),(valorU+valorD)/2]
         return [2,1,3,2]
