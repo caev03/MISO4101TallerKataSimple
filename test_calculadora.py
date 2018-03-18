e822944beedd07cd8c3f4d43f402fd2bbf7fd38a
jshernandez15 <js.hernandez15@uniandes.edu.co>
2018-03-18 16:45:30 -0500

    Paso 2 - pruebas un numero

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,3 +5,6 @@ import Calculadora
 class TestCalculadora(TestCase):
     def test_procesar(self):
         self.assertEqual(Calculadora.Calculadora().procesar(""),[0,"","",""], "Cadena Vacia")
+
+    def test_procesar_numero(self):
+	self.assertEqual([1,1,1,1],Calculadora.Calculadora().procesar("1"),"Un numero")
