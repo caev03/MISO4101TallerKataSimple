ac7f5f42a35cf62f0a33b18372b5dd2d2dcb8da8
caev03 <ca.escobar2434@uniandes.edu.co>
2018-03-18 17:26:57 -0500

    Paso4 - pruebas cadena larga (rojo)

--- Calculadora.py
+++ Calculadora.py
@@ -2,14 +2,14 @@ __author__ = 'caev03'
 
 class Calculadora:
 
-    def procesar(self, cadena):
-        if(len(cadena) == 0):
-            return [0,"","",""]
-        if(len(cadena) == 1):
-	    valor = int(cadena)
-	    return [1, valor, valor, valor]
-	if(len(cadena) == 3):
-	    valorU = int(cadena[0])
-	    valorD = int(cadena[2])
-	    return [2,min(valorU,valorD),max(valorU,valorD),(valorU+valorD)/2]
-        return [2,1,3,2]
+	def procesar(self, cadena):
+		if(len(cadena) == 0):
+			return [0,"","",""]
+		if(len(cadena) == 1):
+			valor = int(cadena)
+			return [1, valor, valor, valor]
+		if(len(cadena) == 3):
+			valorU = int(cadena[0])
+			valorD = int(cadena[2])
+			return [2,min(valorU,valorD),max(valorU,valorD),(valorU+valorD)/2]
+		return ["","","",""]
--- test_calculadora.py
+++ test_calculadora.py
@@ -7,7 +7,10 @@ class TestCalculadora(TestCase):
         self.assertEqual(Calculadora.Calculadora().procesar(""),[0,"","",""], "Cadena Vacia")
 
     def test_procesar_numero(self):
-	self.assertEqual([1,1,1,1],Calculadora.Calculadora().procesar("1"),"Un numero")
+        self.assertEqual([1,1,1,1],Calculadora.Calculadora().procesar("1"),"Un numero")
 
     def test_procesar_dos_numeros(self):
-	self.assertEqual([2,1,3,2],Calculadora.Calculadora().procesar("1,3"),"Dos Numeros")
+        self.assertEqual([2,1,3,2],Calculadora.Calculadora().procesar("1,3"),"Dos Numeros")
+
+    def test_procesar_cadena(self):
+        self.assertEqual([3,1,3,2],Calculadora.Calculadora().procesar("1,2,3"),"Cadena")
