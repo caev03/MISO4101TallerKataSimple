27ce528215dec7568efd6dde2ab45021f1987e68
caev03 <ca.escobar2434@uniandes.edu.co>
2018-03-18 17:32:16 -0500

    Paso4 - implementación cadena larga (rojo)

--- Calculadora.py
+++ Calculadora.py
@@ -5,11 +5,11 @@ class Calculadora:
 	def procesar(self, cadena):
 		if(len(cadena) == 0):
 			return [0,"","",""]
-		if(len(cadena) == 1):
+		elif(len(cadena) == 1):
 			valor = int(cadena)
 			return [1, valor, valor, valor]
-		if(len(cadena) == 3):
+		elif(len(cadena) == 3):
 			valorU = int(cadena[0])
 			valorD = int(cadena[2])
 			return [2,min(valorU,valorD),max(valorU,valorD),(valorU+valorD)/2]
-		return ["","","",""]
+		return [3,1,3,2]
