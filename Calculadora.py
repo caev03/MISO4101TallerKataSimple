673f11dd29d660b8dcb5e8683ffcc6ef129b3db8
caev03 <ca.escobar2434@uniandes.edu.co>
2018-03-18 17:40:03 -0500

    Paso4 - refactoring cadena larga (azul)

--- Calculadora.py
+++ Calculadora.py
@@ -12,4 +12,17 @@ class Calculadora:
 			valorU = int(cadena[0])
 			valorD = int(cadena[2])
 			return [2,min(valorU,valorD),max(valorU,valorD),(valorU+valorD)/2]
-		return [3,1,3,2]
+		else:
+			valores = cadena.split(",")
+			miin = int(valores[0])
+			maax = int(valores[0])
+			promedio = 0
+			for num in valores:
+				nume = int(num)
+				promedio = promedio + nume
+				if nume<miin:
+					miin = nume
+				if nume>maax:
+					maax = nume
+			return [len(valores), miin, maax, promedio/len(valores)]
+		return ["","","",""]
