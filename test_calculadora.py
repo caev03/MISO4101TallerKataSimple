d3ea908e302117af4c2c32e38a552b958e17f9c8
jshernandez15 <js.hernandez15@uniandes.edu.co>
2018-03-18 17:07:27 -0500

    Paso 3 - prueba dos numeros (rojo)

--- test_calculadora.py
+++ test_calculadora.py
@@ -8,3 +8,6 @@ class TestCalculadora(TestCase):
 
     def test_procesar_numero(self):
 	self.assertEqual([1,1,1,1],Calculadora.Calculadora().procesar("1"),"Un numero")
+
+    def test_procesar_dos_numeros(self):
+	self.assertEqual([2,1,3,2],Calculadora.Calculadora().procesar("1,3"),"Dos Numeros")
